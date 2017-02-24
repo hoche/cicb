b98a8ab8fe66c4a63f2408e4d32a2cfa09b9b6b1
Michel Hoche-Mong <hoche@grok.com>
2017-02-23 22:09:14 -0800

    Wrap "compiled with" stuff with HAVE_BLAH macros

--- icb/c_version.c
+++ icb/c_version.c
@@ -1,24 +1,29 @@
-/* $Id: c_version.c,v 1.9 2009/04/04 09:08:40 hoche Exp $ */
-
 /* primitive to display client version info */
 #include "icb.h"
 
+#ifdef HAVE_READLINE
 #include <readline/readline.h>
+#endif
 
 int
 c_version (ARGV_TCL)
 {
 	static char *usage = "usage: c_version";
-    char buf[256];
+    char buf[BUFSIZ];
 
 	/* check the arg count */
 	if (argc != 1)
 		TRETURNERR(usage)
 
 	putl(icb_version, PL_SCR);
-	sprintf(buf, "Built with readline %s and TCL %s",
-		rl_library_version,
-		TCL_PATCH_LEVEL);
+#ifdef HAVE_READLINE
+    snprintf(buf, BUFSIZ, "   readline %s", rl_library_version);
 	putl(buf, PL_SCR);
+#endif
+#ifdef HAVE_TCL
+    snprintf(buf, BUFSIZ, "   TCL %s", TCL_PATCH_LEVEL);
+	putl(buf, PL_SCR);
+#endif
+
 	return(TCL_OK);
 }
